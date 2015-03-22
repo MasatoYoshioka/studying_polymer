8efe6822fe620f5f8149d2fd75ad4fa0496ee243
Masato Yoshioka <yoshioka0627@gmail.com>
2015-03-22 16:26:23 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+//Gulp
+var gulp    = require('gulp');
+//gulp実行時にエラーが出ても処理が中断しない
+var plumber = require('gulp-plumber');
+//JS圧縮
+var uglify  = require('gulp-uglify');
+//構文チェック
+var jshint  = require('gulp-jshint');
+
+
+
